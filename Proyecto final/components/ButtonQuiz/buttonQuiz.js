ced98c5eaaeabab8b183e430cc5c7fe6849c70c2
abelgh91 <abelgh91@gmail.com>
2024-01-04 15:49:57 +0100

    arreglos primer proyecto

--- Proyecto final/components/ButtonQuiz/buttonQuiz.js
+++ Proyecto final/components/ButtonQuiz/buttonQuiz.js
@@ -1,25 +1,17 @@
-const getPreviousIndex = () => {
-    console.log("index en el previus", index);
-    if (index >= 1) {
-      index--;
-    }
-  
-    if (index === 0) {
-      const navigatePrevious = document.getElementById("previous");
-      navigatePrevious.style.display = "none";
-    }
-  };
-  
-  const getNextIndex = () => {
-    index++;
-    if (index === 0) {
-      const navigatePrevious = document.getElementById("previous");
-      navigatePrevious.style.display = "none";
-    }
-    if (index > 0) {
-      const navigatePrevious = document.getElementById("previous");
-      navigatePrevious.style.display = "flex";
-    }
-  };
+let index = 0;
 
-  module.exports = {getPreviousIndex, getNextIndex}
+export const getPreviousIndex = () => {
+  console.log("index en el previus", index);
+  if (index > 0) {
+    index--;
+  }
+  
+    return index;
+};
+
+export const getNextIndex = (totalQuestions) => {
+  if (index < totalQuestions - 1) {
+    index++;
+  }
+  return index;
+};
--- Proyecto final/pages/Quiz/Quiz.js
+++ Proyecto final/pages/Quiz/Quiz.js
@@ -1,7 +1,127 @@
 
 import { getNextIndex, getPreviousIndex } from "../../components/ButtonQuiz/buttonQuiz";
 import "./Quiz.css"
-import preguntasRespuestas from "./questions";
+
+const preguntasRespuestas = [{
+  "pregunta": "1.¿Cuál es el primer club de fútbol de la historia?",
+  "respuesta1": "Recreativo de Huelva",
+  "respuesta2": "Independiente de Avellaneda",
+  "respuesta3": "Sheffield Football Club",
+  "respuesta4": "Santos FC",
+  "correcta" : "respuesta3"
+},
+{
+  "pregunta": "2.¿Cuántos segundos tardó Ricaro Oliveira en marcar el gol más rápido de la historia?",
+  "respuesta1": "2.8",
+  "respuesta2": "3.5",
+  "respuesta3": "5.4",
+  "respuesta4": "4.8",
+  "correcta" : "respuesta1"
+},
+{
+  "pregunta": "3.¿Cuál de estos futbolistas tiene 3 mundiales?",
+  "respuesta1": "Zinedine Zidane",
+  "respuesta2": "Pelé",
+  "respuesta3": "Leo Messi",
+  "respuesta4": "Maradona",
+  "correcta" : "respuesta2"
+},
+{
+  "pregunta": "4.¿Qué jugador ha jugado más partidos en la liga española?",
+  "respuesta1": "Xavi Hernández",
+  "respuesta2": "Joaquín Sánchez",
+  "respuesta3": "Raúl González",
+  "respuesta4": "Andoni Zubizarreta",
+  "correcta" : "respuesta4"
+},
+{
+  "pregunta": "5.¿Sabes cuántas victorias ha conseguido San Marino hasta hoy en 106 partidos?",
+  "respuesta1": "11",
+  "respuesta2": "4",
+  "respuesta3": "1",
+  "respuesta4": "9",
+  "correcta" : "respuesta3"
+},
+{
+  "pregunta": "6.¿Quién es el único portero hasta el momento en conseguir el Balón de Oro?",
+  "respuesta1": "Gianluigi Buffon",
+  "respuesta2": "Oliver Khan",
+  "respuesta3": "Iker Casillas",
+  "respuesta4": "Ter Stegen",
+  "correcta" : "respuesta2"
+},
+{
+  "pregunta": "7.Rogerio Ceni, es el portero con más goles anotados de la historia, ¿sabes cuántos anotó?",
+  "respuesta1": "59",
+  "respuesta2": "8",
+  "respuesta3": "254",
+  "respuesta4": "128",
+  "correcta" : "respuesta4"
+},
+{
+  "pregunta": "8.¿Cuál es el traspaso más caro de la historia del fútbol?",
+  "respuesta1": "Cristiano Ronaldo al Real Madrid",
+  "respuesta2": "Neymar al PSG",
+  "respuesta3": "Coutinho al Barcelona",
+  "respuesta4": "Mbappé al Real Madrid",
+  "correcta" : "respuesta2"
+},
+{
+  "pregunta": "9.¿Sabrías decir en cuántos equipos jugó el Loco Abreu en 26 años?",
+  "respuesta1": "31",
+  "respuesta2": "26",
+  "respuesta3": "15",
+  "respuesta4": "42",
+  "correcta" : "respuesta1"
+},
+{
+  "pregunta": "10.¿Cuál es el estadio más antiguo del mundo?",
+  "respuesta1": "Parque de los Príncipes",
+  "respuesta2": "Hampden Park",
+  "respuesta3": "Bramall Lane",
+  "respuesta4": "Anfield",
+  "correcta" : "respuesta3"
+},
+{
+  "pregunta": "11.¿Qué selección tiene el récord de mayor número de partidos invictos?",
+  "respuesta1": "España",
+  "respuesta2": "Argentina",
+  "respuesta3": "Italia",
+  "respuesta4": "Brasil",
+  "correcta" : "respuesta3"
+},
+{
+  "pregunta": "12.¿Cuánto pesa un balón de fútbol?",
+  "respuesta1": "Entre 550-600 gramos",
+  "respuesta2": "Entre 340-390 gramos",
+  "respuesta3": "Entre 460-510 gramos",
+  "respuesta4": "Entre 410-450 gramos",
+  "correcta" : "respuesta4"
+},
+{
+  "pregunta": "13.¿Qué jugador ha disputado más partidos con la selección española?",
+  "respuesta1": "Sergio Ramos",
+  "respuesta2": "Xavi Hernández",
+  "respuesta3": "Iker Casillas",
+  "respuesta4": "Carles Puyol",
+  "correcta" : "respuesta1"
+},
+{
+  "pregunta": "14.¿Cuál de estos equipos NO tiene una Champions League?",
+  "respuesta1": "Hamburgo",
+  "respuesta2": "Aston Villa",
+  "respuesta3": "Celtic de Glasgow",
+  "respuesta4": "PSG",
+  "correcta" : "respuesta4"
+},
+{
+  "pregunta": "15.¿A quién nos referimos si hablamos del Rubio de Barakaldo?",
+  "respuesta1": "Xabi Alonso",
+  "respuesta2": "Julen Guerrero",
+  "respuesta3": "Javier Clemente",
+  "respuesta4": "Julen Lopetegui",
+  "correcta" : "respuesta3"
+},]
 
 
 let index = 0;
@@ -24,17 +144,25 @@ let preguntasRespondidas = [];
   </div>
 `;
 
+const updatePreviousButtonVisibility = () => {
+  const navigatePrevious = document.getElementById("previous");
+  navigatePrevious.style.display = index === 0 ? "none" : "flex";
+};
+
 const addEventListeners = () => {
   const navigatePrevious = document.getElementById("previous");
   navigatePrevious.addEventListener("click", () => {
-    getPreviousIndex();
-    loadQuestion();
-  });
+    if (index > 0) {
+      index = getPreviousIndex();
+      loadQuestion();
+      updatePreviousButtonVisibility();
+  }});
 
   const navigateNext = document.getElementById("next");
   navigateNext.addEventListener("click", () => {
-    getNextIndex();
+    index = getNextIndex(preguntasRespuestas.length);
     loadQuestion();
+    updatePreviousButtonVisibility();
   });
 
   const respuestaUno = document.getElementById("r1");
@@ -59,7 +187,6 @@ const addEventListeners = () => {
 };
 
 export const PintarQuiz = () => {
-  index = 0;
   preguntasRespondidas = [];
   document.querySelector("main").innerHTML = template();
   document.querySelector("nav").style.display = "flex";
@@ -71,7 +198,7 @@ export const PintarQuiz = () => {
   congratulationsTemplate();
 };
 
-const congratulationsTemplate = (score) => `
+const congratulationsTemplate = () => `
 <div id="Enhorabuena">
 <p>Enhorabuena!! Has respondido ${score} de 15 preguntas correctamente</p>
 <button id="restartButton">Volver a jugar</button>
@@ -79,8 +206,7 @@ const congratulationsTemplate = (score) => `
 `;
 
 const loadQuestion = () => {
-
-  if (index >= preguntasRespuestas.length) {
+  if (index === preguntasRespuestas.length) {
     document.querySelector("#Quiz").style.display = "none";
     const scoreElement = document.querySelector(".score");
     const congratulationsElement = document.createElement("div");
@@ -97,27 +223,28 @@ const loadQuestion = () => {
       congratulationsElement.remove();
       PintarQuiz();
     });
+
     return;
   }
 
-  let buttons = document.querySelectorAll(".answer-btn");
+  const buttons = document.querySelectorAll(".answer-btn");
   buttons.forEach((button) => {
     button.style.backgroundColor = "";
     button.removeAttribute("disabled");
   });
 
-  document.getElementById("question").innerText =
-    preguntasRespuestas[index].pregunta;
-  document.getElementById("r1").innerText =
-    preguntasRespuestas[index].respuesta1;
-  document.getElementById("r2").innerText =
-    preguntasRespuestas[index].respuesta2;
-  document.getElementById("r3").innerText =
-    preguntasRespuestas[index].respuesta3;
-  document.getElementById("r4").innerText =
-    preguntasRespuestas[index].respuesta4;
-  document.getElementById("solution").innerText = "";
-  preguntasRespondidas.push(index);
+  const currentQuestion = preguntasRespuestas[index];
+  if (currentQuestion) {
+    document.getElementById("question").innerText = currentQuestion.pregunta;
+    document.getElementById("r1").innerText = currentQuestion.respuesta1;
+    document.getElementById("r2").innerText = currentQuestion.respuesta2;
+    document.getElementById("r3").innerText = currentQuestion.respuesta3;
+    document.getElementById("r4").innerText = currentQuestion.respuesta4;
+    document.getElementById("solution").innerText = "";
+    preguntasRespondidas.push(index);
+  } else {
+    console.error(`La pregunta en la posición ${index} es undefined.`);
+  }
 };
 
 const checkAnswer = (answer) => {
@@ -143,6 +270,7 @@ const checkAnswer = (answer) => {
     document.getElementById("solution").style.color = "green";
     score++;
     document.getElementById("score").innerText = score;
+    console.log("indice actual:", index)
   } else {
     document.getElementById("solution").innerText = "INCORRECTO";
     document.getElementById("solution").style.color = "red";
@@ -150,4 +278,4 @@ const checkAnswer = (answer) => {
 };
 congratulationsTemplate();
 
-console.log(preguntasRespondidas);
+export const preguntas = () => preguntasRespuestas();
